00aad10219990e516cc649d9aa7312eaa36183be
Ariana-Elahi <ejq5jv@virginia.edu>
2025-10-22 12:16:28 -0400

    Delete Project2-DataCleaning.R

--- Project2-DataCleaning.R
+++
@@ -1,8 +0,0 @@
-#Cleaning data
-
-data <- read.csv("/Users/arianaelahi/Desktop/OriginalAirQualityUCI.csv",
-                 sep = ";", dec = ",", na.strings = c("NA", "-200"))
-names(data) <- make.names(names(data), unique = TRUE)
-
-write.csv(data, "/Users/arianaelahi/Desktop/cleaned_air_quality.csv", row.names = FALSE)
-
